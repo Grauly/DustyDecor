35e237a36758013739c3972604a933932668a327
Grauly <nils.dellemann@web.de>
2025-07-29 00:48:01 +0200

    feat: add climbing in vents

--- src/datagen/kotlin/grauly/dustydecor/BlockTagDatagen.kt
+++ src/datagen/kotlin/grauly/dustydecor/BlockTagDatagen.kt
@@ -3,6 +3,7 @@ package grauly.dustydecor
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
 import net.minecraft.registry.RegistryWrapper
+import net.minecraft.registry.tag.BlockTags
 import java.util.concurrent.CompletableFuture
 
 class BlockTagDatagen(
@@ -14,6 +15,9 @@ class BlockTagDatagen(
         valueLookupBuilder(ModBlockTags.LARGE_VENT_CONNECTABLE).add(
             ModBlocks.VENT,
             ModBlocks.VENT_COVER
+        )
+        valueLookupBuilder(BlockTags.CLIMBABLE).add(
+            ModBlocks.VENT
         )
     }
 }
