54f61c40b76327d71a4e0c215eca4e4cf1371245
Grauly <nils.dellemann@web.de>
2025-07-29 00:41:34 +0200

    feat: forced swimming inside of vents

---
+++ src/main/java/grauly/dustydecor/mixin/PlayerEntityVentCrawlingMixin.java
@@ -0,0 +1,36 @@
+package grauly.dustydecor.mixin;
+
+import grauly.dustydecor.DustyDecorMod;
+import grauly.dustydecor.ModBlocks;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityVentCrawlingMixin {
+
+    @Unique
+    private boolean isPlayerInVent(PlayerEntity player) {
+        return player.getWorld().getBlockState(player.getBlockPos()).isOf(ModBlocks.INSTANCE.getVENT());
+    }
+
+    @Inject(method = "canChangeIntoPose", at = @At("HEAD"), cancellable = true)
+    private void forceVentCrawling(EntityPose pose, CallbackInfoReturnable<Boolean> cir) {
+        if (pose == EntityPose.SWIMMING) {
+            if (isPlayerInVent(((PlayerEntity) (Object) this))) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+
+    @Inject(method = "getExpectedPose", at = @At("HEAD"), cancellable = true)
+    private void expectVentCrawling(CallbackInfoReturnable<EntityPose> cir) {
+        if (isPlayerInVent(((PlayerEntity) (Object) this))) {
+            cir.setReturnValue(EntityPose.SWIMMING);
+        }
+    }
+}
