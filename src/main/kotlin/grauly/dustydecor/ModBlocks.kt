8e266ada36dca4791e02f2e8648d8838077d9a45
Grauly <nils.dellemann@web.de>
2025-07-29 17:47:07 +0200

    feat: better block settings

--- src/main/kotlin/grauly/dustydecor/ModBlocks.kt
+++ src/main/kotlin/grauly/dustydecor/ModBlocks.kt
@@ -4,6 +4,7 @@ import grauly.dustydecor.block.VentBlock
 import grauly.dustydecor.block.VentCoverBlock
 import net.minecraft.block.AbstractBlock.Settings
 import net.minecraft.block.Block
+import net.minecraft.block.Blocks
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
@@ -12,8 +13,8 @@ import net.minecraft.util.Identifier
 
 object ModBlocks {
 
-    val VENT: Block = registerBlock(::VentBlock, "vent")
-    val VENT_COVER: Block = registerBlock(::VentCoverBlock, "vent_cover", Settings.create().ticksRandomly().nonOpaque())
+    val VENT: Block = registerBlock(::VentBlock, "vent", Settings.copy(Blocks.IRON_BLOCK))
+    val VENT_COVER: Block = registerBlock(::VentCoverBlock, "vent_cover", Settings.copy(Blocks.IRON_TRAPDOOR).ticksRandomly().nonOpaque())
 
     private fun registerBlock(
         blockFactory: (Settings) -> Block,
