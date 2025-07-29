ed8458bec817e49bec59ae9a65ec346619c0fcb8
Grauly <nils.dellemann@web.de>
2025-07-29 23:38:58 +0200

    feat: better auto closing behavior

--- src/main/kotlin/grauly/dustydecor/block/VentCoverBlock.kt
+++ src/main/kotlin/grauly/dustydecor/block/VentCoverBlock.kt
@@ -1,37 +1,52 @@
 package grauly.dustydecor.block
 
-import grauly.dustydecor.ModBlocks
 import net.minecraft.block.Block
 import net.minecraft.block.BlockSetType
 import net.minecraft.block.BlockState
 import net.minecraft.block.TrapdoorBlock
-import net.minecraft.block.enums.BlockHalf
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.EnumProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.random.Random
 
-class VentCoverBlock(settings: Settings): TrapdoorBlock(BlockSetType.COPPER, settings) {
+class VentCoverBlock(settings: Settings) : TrapdoorBlock(BlockSetType.COPPER, settings) {
+
+    init {
+        defaultState = defaultState
+            .with(Properties.LOCKED, false)
+            .with(COVERS_FACE, Direction.UP)
+    }
+
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
+        val directionPlacedAgainst = ctx.side
+        return super.getPlacementState(ctx)
+            ?.with(COVERS_FACE, directionPlacedAgainst.opposite)
+            ?.with(OPEN, !directionPlacedAgainst.axis.isHorizontal)
+    }
+
     override fun randomTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: Random) {
-        val isConnectedToVentSideways = world.getBlockState(pos.offset(state.get(FACING).opposite)).isOf(ModBlocks.VENT)
-        val isConnectedToVentAbove = world.getBlockState(pos.offset(Direction.UP)).isOf(ModBlocks.VENT)
-        val isConnectedToVentBelow = world.getBlockState(pos.offset(Direction.DOWN)).isOf(ModBlocks.VENT)
-        if (isConnectedToVentSideways && (isConnectedToVentAbove || isConnectedToVentBelow)) return //Cannot determine target state reliably
-        if (isConnectedToVentSideways && state.get(HALF).equals(BlockHalf.TOP) && !state.get(OPEN)) {
-            world.setBlockState(pos, state.with(OPEN, true), Block.NOTIFY_LISTENERS)
+        //target state is open = true for horizontal covers, and open = false for vertical covers
+        val isHorizontal = state.get(COVERS_FACE).axis.isHorizontal
+        if (!state.get(OPEN) && isHorizontal || state.get(OPEN) && !isHorizontal) {
+            world.setBlockState(pos, state.with(OPEN, !state.get(OPEN)), Block.NOTIFY_LISTENERS)
             this.playToggleSound(null, world, pos, true)
-        }
-        if (isConnectedToVentAbove && state.get(HALF).equals(BlockHalf.TOP) && state.get(OPEN)) {
-            world.setBlockState(pos, state.with(OPEN, false), Block.NOTIFY_LISTENERS)
-            this.playToggleSound(null, world, pos, false)
-        }
-        if (isConnectedToVentBelow && state.get(HALF).equals(BlockHalf.BOTTOM) && state.get(OPEN)) {
-            world.setBlockState(pos, state.with(OPEN, false), Block.NOTIFY_LISTENERS)
-            this.playToggleSound(null, world, pos, false)
         }
     }
 
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        super.appendProperties(builder)
+        builder.add(Properties.LOCKED, COVERS_FACE)
+    }
+
     override fun hasRandomTicks(state: BlockState): Boolean {
-        return super.hasRandomTicks(state) && !state.get(POWERED, false)
+        return super.hasRandomTicks(state) && !state.get(POWERED, false) && !state.get(Properties.LOCKED)
+    }
+
+    companion object {
+        val COVERS_FACE: EnumProperty<Direction> = EnumProperty.of("covers", Direction::class.java)
     }
 }
