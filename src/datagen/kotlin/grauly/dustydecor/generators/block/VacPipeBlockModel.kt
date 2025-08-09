903c7bea1d02d7d229ecfde6fff1b0acd2ba76b1
Grauly <nils.dellemann@web.de>
2025-08-09 22:05:27 +0200

    refactor: update core generation

--- src/datagen/kotlin/grauly/dustydecor/generators/block/VacPipeBlockModel.kt
+++ src/datagen/kotlin/grauly/dustydecor/generators/block/VacPipeBlockModel.kt
@@ -7,11 +7,7 @@ import grauly.dustydecor.block.VacPipeBlock
 import grauly.dustydecor.generators.BlockModelDatagen
 import net.minecraft.client.data.BlockStateModelGenerator
 import net.minecraft.client.data.MultipartBlockModelDefinitionCreator
-import net.minecraft.client.render.model.json.ModelVariantOperator
-import net.minecraft.client.render.model.json.MultipartModelCombinedCondition
-import net.minecraft.client.render.model.json.MultipartModelCondition
-import net.minecraft.client.render.model.json.MultipartModelConditionBuilder
-import net.minecraft.client.render.model.json.WeightedVariant
+import net.minecraft.client.render.model.json.*
 import net.minecraft.util.math.Direction
 
 object VacPipeBlockModel {
@@ -43,7 +39,7 @@ object VacPipeBlockModel {
             //Transparent Variant
             val transparentCondition = MultipartModelConditionBuilder()
                 .put(it, ConnectionState.fromDirection(direction))
-            AbConnectableBlock.connections.forEach { f -> transparentCondition.put(VacPipeBlock.windowMap[f], true)}
+            AbConnectableBlock.connections.forEach { f -> transparentCondition.put(VacPipeBlock.windowMap[f], true) }
             creator.with(
                 transparentCondition,
                 VAC_CONNECTOR_TRANSPARENT
@@ -52,12 +48,17 @@ object VacPipeBlockModel {
             )
 
             //Window Variant
-            val orList: List<MultipartModelCondition> = AbConnectableBlock.connections.filter { f -> f != it }.map { f -> MultipartModelConditionBuilder().put(VacPipeBlock.windowMap[f], false).build() }
+            val orList: List<MultipartModelCondition> = AbConnectableBlock.connections.filter { f -> f != it }
+                .map { f -> MultipartModelConditionBuilder().put(VacPipeBlock.windowMap[f], false).build() }
             val directionCondition = MultipartModelConditionBuilder()
                 .put(it, ConnectionState.fromDirection(direction))
                 .build()
-            val orCondition = MultipartModelCombinedCondition(MultipartModelCombinedCondition.LogicalOperator.OR, orList)
-            val windowCondition = MultipartModelCombinedCondition(MultipartModelCombinedCondition.LogicalOperator.AND, listOf(directionCondition, orCondition))
+            val orCondition =
+                MultipartModelCombinedCondition(MultipartModelCombinedCondition.LogicalOperator.OR, orList)
+            val windowCondition = MultipartModelCombinedCondition(
+                MultipartModelCombinedCondition.LogicalOperator.AND,
+                listOf(directionCondition, orCondition)
+            )
             creator.with(
                 windowCondition,
                 VAC_CONNECTOR_WINDOW
@@ -71,28 +72,70 @@ object VacPipeBlockModel {
         creator: MultipartBlockModelDefinitionCreator,
         aState: ConnectionState
     ) {
-        val connections = (ModBlocks.VAC_PIPE as VacPipeBlock).getConnections()
+        val connections = AbConnectableBlock.connections
+
+        //Base, full opaque
+        val falseOrList: List<MultipartModelCondition> = AbConnectableBlock.connections.map { f ->
+            MultipartModelConditionBuilder().put(
+                VacPipeBlock.windowMap[f],
+                false
+            ).build()
+        }
+        val anyNotWindowCondition =
+            MultipartModelCombinedCondition(MultipartModelCombinedCondition.LogicalOperator.OR, falseOrList)
         for (bState: ConnectionState in ConnectionState.entries) {
             creator.with(
-                MultipartModelConditionBuilder()
-                    .put(connections[0], aState)
-                    .put(connections[1], bState),
-                if (aState == ConnectionState.NONE || bState == ConnectionState.NONE) {
-                    VAC_CORE
-                } else if (aState.direction?.opposite!! == bState.direction!!) {
-                    VAC_CORE_STRAIGHT
-                        .apply(ModelVariantOperator.UV_LOCK.withValue(true))
-                        .apply(BlockModelDatagen.NORTH_FACING_ROTATION_MAP[aState.direction])
-                } else {
-                    VAC_CORE
+                MultipartModelCombinedCondition(
+                    MultipartModelCombinedCondition.LogicalOperator.AND, listOf(
+                        MultipartModelConditionBuilder()
+                            .put(connections[0], aState)
+                            .put(connections[1], bState)
+                            .build(),
+                        anyNotWindowCondition
+                    )
+                ),
+                VAC_CORE
+            )
+        }
+
+        //Window and transparent, aka transparent, but around corner
+        val trueAndList: List<MultipartModelCondition> = AbConnectableBlock.connections.map { f ->
+            MultipartModelConditionBuilder().put(VacPipeBlock.windowMap[f], true).build()
+        }
+        val allWindowCondition =
+            MultipartModelCombinedCondition(MultipartModelCombinedCondition.LogicalOperator.AND, trueAndList)
+        for (bState: ConnectionState in ConnectionState.entries) {
+            creator.with(
+                MultipartModelCombinedCondition(
+                    MultipartModelCombinedCondition.LogicalOperator.AND, listOf(
+                        MultipartModelConditionBuilder()
+                            .put(connections[0], aState)
+                            .put(connections[1], bState)
+                            .build(),
+                        allWindowCondition
+                    )
+                ),
+                run {
+                    if (aState != ConnectionState.NONE && bState != ConnectionState.NONE) {
+                        if (aState.direction!!.opposite == bState.direction) {
+                            return@run VAC_CORE_TRANSPARENT
+                                .apply(ModelVariantOperator.UV_LOCK.withValue(true))
+                                .apply(BlockModelDatagen.NORTH_FACING_ROTATION_MAP[aState.direction])
+                        }
+                    }
+                    return@run VAC_CORE_WINDOW
                 }
             )
         }
     }
 
     private val VAC_CORE: WeightedVariant = BlockModelDatagen.singleVariant("block/vac_pipe_core")
+    private val VAC_CORE_WINDOW: WeightedVariant = BlockModelDatagen.singleVariant("block/vac_pipe_core_window")
+    private val VAC_CORE_TRANSPARENT: WeightedVariant =
+        BlockModelDatagen.singleVariant("block/vac_pipe_core_transparent")
     private val VAC_CONNECTOR: WeightedVariant = BlockModelDatagen.singleVariant("block/vac_pipe_connector")
-    private val VAC_CONNECTOR_WINDOW: WeightedVariant = BlockModelDatagen.singleVariant("block/vac_pipe_connector_window")
-    private val VAC_CONNECTOR_TRANSPARENT: WeightedVariant = BlockModelDatagen.singleVariant("block/vac_pipe_connector_transparent")
-    private val VAC_CORE_STRAIGHT: WeightedVariant = BlockModelDatagen.singleVariant("block/vac_pipe_core_straight")
+    private val VAC_CONNECTOR_WINDOW: WeightedVariant =
+        BlockModelDatagen.singleVariant("block/vac_pipe_connector_window")
+    private val VAC_CONNECTOR_TRANSPARENT: WeightedVariant =
+        BlockModelDatagen.singleVariant("block/vac_pipe_connector_transparent")
 }
