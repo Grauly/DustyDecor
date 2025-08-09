c995698792e15d7260ada819dabff68f6f36beb0
Grauly <nils.dellemann@web.de>
2025-08-09 16:38:50 +0200

    feat: add blockpos naming debug utils

---
+++ src/main/kotlin/grauly/dustydecor/util/DebugUtils.kt
@@ -0,0 +1,11 @@
+package grauly.dustydecor.util
+
+import net.minecraft.util.math.BlockPos
+
+object DebugUtils {
+    const val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    private val nameMap: MutableMap<BlockPos, String> = mutableMapOf()
+    fun nameBlockPos(pos: BlockPos): String = nameMap.computeIfAbsent(pos, {
+        "${letters[nameMap.size % letters.length]}${nameMap.size / letters.length}"
+    })
+}
