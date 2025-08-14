3b7ca9fc1727c9663707eccbf25379baa504d204
Grauly <nils.dellemann@web.de>
2025-08-15 01:36:42 +0200

    feat: working pipe core rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations
    fuck rotations

--- src/datagen/kotlin/grauly/dustydecor/generators/block/VacPipeBlockModel.kt
+++ src/datagen/kotlin/grauly/dustydecor/generators/block/VacPipeBlockModel.kt
@@ -7,11 +7,12 @@ import grauly.dustydecor.block.VacPipeBlock
 import grauly.dustydecor.generators.BlockModelDatagen
 import net.minecraft.client.data.BlockStateModelGenerator
 import net.minecraft.client.data.MultipartBlockModelDefinitionCreator
-import net.minecraft.client.render.model.json.ModelVariantOperator
-import net.minecraft.client.render.model.json.MultipartModelCombinedCondition
-import net.minecraft.client.render.model.json.MultipartModelCondition
-import net.minecraft.client.render.model.json.MultipartModelConditionBuilder
+import net.minecraft.client.render.model.json.*
 import net.minecraft.state.property.EnumProperty
+import net.minecraft.util.math.AxisRotation
+import net.minecraft.util.math.Direction
+import kotlin.math.abs
+import kotlin.math.sign
 
 object VacPipeBlockModel {
     fun get(blockStateModelGenerator: BlockStateModelGenerator) {
@@ -118,9 +119,7 @@ object VacPipeBlockModel {
                 if (!isStraight) {
                     creator.with(
                         directionCondition,
-                        VAC_CORE_NORTH_TOP_OPAQUE
-                            .apply(BlockModelDatagen.NORTH_FACING_ROTATION_MAP[aDirection.fallDown!!])
-                            .apply(BlockModelDatagen.TOP_FACING_ROTATION_MAP[bDirection.fallDown!!])
+                        getMultiRotationCore(aDirection.direction!!, bDirection.direction!!)
                             .apply(uvLock(false))
                     )
                 } else {
@@ -158,6 +157,58 @@ object VacPipeBlockModel {
         )
     }
 
+    private fun getMultiRotationCore(aDirection: Direction, bDirection: Direction): WeightedVariant {
+        val baseVariant =
+            if (isReachableViaXRotation(aDirection) && isReachableViaXRotation(bDirection)) VAC_CORE_NORTH_TOP_OPAQUE else VAC_CORE_NORTH_EAST_OPAQUE
+        return baseVariant.apply(getMultiRotation(aDirection, bDirection))
+    }
+
+    //Problem: MC applies X rotation before Y rotation, always
+    private fun getMultiRotation(aDirection: Direction, bDirection: Direction): ModelVariantOperator {
+        if (aDirection == bDirection) return BlockModelDatagen.NORTH_FACING_ROTATION_MAP[aDirection]!!
+        if (isReachableViaXRotation(aDirection) && isReachableViaXRotation(bDirection))
+            return rotatePlanarX(aDirection, bDirection)
+        if (isReachableViaYRotation(aDirection) && isReachableViaYRotation(bDirection))
+            return rotatePlanarY(aDirection, bDirection)
+        return rotatePlanarZ(aDirection, bDirection)
+    }
+
+    private fun rotatePlanarZ(aDirection: Direction, bDirection: Direction): ModelVariantOperator {
+        //assuming N-E Connector
+        val xRotation = if (aDirection == Direction.DOWN || bDirection == Direction.DOWN) AxisRotation.R90 else AxisRotation.R270
+        val yRotation = if (aDirection == Direction.WEST || bDirection == Direction.WEST) AxisRotation.R180 else AxisRotation.R0
+        return ModelVariantOperator.ROTATION_X.withValue(xRotation).then(ModelVariantOperator.ROTATION_Y.withValue(yRotation))
+    }
+
+    private fun rotatePlanarY(aDirection: Direction, bDirection: Direction): ModelVariantOperator {
+        //eliminate isomorphisms, and guarantee that the smaller rotation is aDirection
+        if (yRotationDirections.indexOf(bDirection) < yRotationDirections.indexOf(aDirection))
+            return rotatePlanarY(bDirection, aDirection)
+        //assuming N-E Connector, aDirection is the smaller rotation of both
+        if (aDirection == Direction.NORTH && bDirection == Direction.WEST) {
+            return ModelVariantOperator.ROTATION_Y.withValue(AxisRotation.R270)
+        }
+        return ModelVariantOperator.ROTATION_Y.withValue(rotationOffsets[yRotationDirections.indexOf(aDirection)])
+    }
+
+    private fun rotatePlanarX(aDirection: Direction, bDirection: Direction): ModelVariantOperator {
+        //eliminate isomorphisms, and guarantee that aRotation < bRotation
+        if (xRotationDirections.indexOf(bDirection) < xRotationDirections.indexOf(aDirection)) {
+            return rotatePlanarX(bDirection, aDirection)
+        }
+        //assuming N-T Connector, aDirection is the smaller rotation of both
+        if (aDirection == Direction.NORTH && bDirection == Direction.UP) {
+            return ModelVariantOperator.ROTATION_X.withValue(AxisRotation.R0)
+        }
+        return ModelVariantOperator.ROTATION_X.withValue(rotationOffsets[xRotationDirections.indexOf(bDirection)])
+    }
+
+    private val rotationOffsets = listOf(AxisRotation.R0, AxisRotation.R90, AxisRotation.R180, AxisRotation.R270)
+    private val xRotationDirections = listOf(Direction.NORTH, Direction.DOWN, Direction.SOUTH, Direction.UP) //CCW
+    private val yRotationDirections = listOf(Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST) //CW
+    private fun isReachableViaXRotation(direction: Direction): Boolean = xRotationDirections.contains(direction)
+    private fun isReachableViaYRotation(direction: Direction): Boolean = yRotationDirections.contains(direction)
+
     //again, bc I messed that up before: true means move the uv (lock the result to original rotation of the image), false means keep the original UV mapping
     private fun uvLock(lock: Boolean): ModelVariantOperator? {
         return ModelVariantOperator.UV_LOCK.withValue(lock)
@@ -175,6 +226,7 @@ object VacPipeBlockModel {
     private val VAC_CORE_STRAIGHT_TRANSPARENT =
         BlockModelDatagen.singleVariant("block/vac_pipe_straight_core_transparent")
     private val VAC_CORE_NORTH_TOP_OPAQUE = BlockModelDatagen.singleVariant("block/vac_pipe_north_top_core_opaque")
+    private val VAC_CORE_NORTH_EAST_OPAQUE = BlockModelDatagen.singleVariant("block/vac_pipe_north_east_core_opaque")
     private val VAC_CORE_NORTH_OPAQUE = BlockModelDatagen.singleVariant("block/vac_pipe_north_core_opaque")
     private val VAC_CORE_NONE_OPAQUE = BlockModelDatagen.singleVariant("block/vac_pipe_none_core_opaque")
     private val VAC_CONNECTOR_OPAQUE = BlockModelDatagen.singleVariant("block/vac_pipe_connector_opaque")
