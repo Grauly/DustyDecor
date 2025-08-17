40093788a3593484f1de916cb6c1bcdec1578804
Grauly <nils.dellemann@web.de>
2025-08-18 00:45:27 +0200

    feat: add dye item index

--- src/main/kotlin/grauly/dustydecor/util/DyeUtils.kt
+++ src/main/kotlin/grauly/dustydecor/util/DyeUtils.kt
@@ -1,5 +1,6 @@
 package grauly.dustydecor.util
 
+import net.minecraft.item.Items
 import net.minecraft.util.DyeColor
 
 object DyeUtils {
@@ -19,5 +20,23 @@ object DyeUtils {
         DyeColor.PURPLE,
         DyeColor.MAGENTA,
         DyeColor.PINK
+    )
+
+    val DYE_ORDER = listOf(
+        Items.WHITE_DYE,
+        Items.LIGHT_GRAY_DYE,
+        Items.GRAY_DYE,
+        Items.BLACK_DYE,
+        Items.BROWN_DYE,
+        Items.RED_DYE,
+        Items.ORANGE_DYE,
+        Items.YELLOW_DYE,
+        Items.LIME_DYE,
+        Items.GREEN_DYE,
+        Items.CYAN_DYE,
+        Items.LIGHT_BLUE_DYE,
+        Items.PURPLE_DYE,
+        Items.MAGENTA_DYE,
+        Items.PINK_DYE
     )
 }
