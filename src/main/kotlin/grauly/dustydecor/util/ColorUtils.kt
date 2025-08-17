4de94c83a5161156a76a9203237b65360cc15b85
Grauly <nils.dellemann@web.de>
2025-08-18 00:28:13 +0200

    feat: add ColorUtils

---
+++ src/main/kotlin/grauly/dustydecor/util/ColorUtils.kt
@@ -0,0 +1,23 @@
+package grauly.dustydecor.util
+
+import net.minecraft.util.DyeColor
+
+object ColorUtils {
+    val COLOR_ORDER = listOf(
+        DyeColor.WHITE,
+        DyeColor.LIGHT_GRAY,
+        DyeColor.GRAY,
+        DyeColor.BLACK,
+        DyeColor.BROWN,
+        DyeColor.RED,
+        DyeColor.ORANGE,
+        DyeColor.YELLOW,
+        DyeColor.LIME,
+        DyeColor.GREEN,
+        DyeColor.CYAN,
+        DyeColor.LIGHT_BLUE,
+        DyeColor.PURPLE,
+        DyeColor.MAGENTA,
+        DyeColor.PINK
+    )
+}
