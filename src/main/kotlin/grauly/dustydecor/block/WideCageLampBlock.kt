b28b37b5983cf3355f1137060f34b4f81625818b
Grauly <nils.dellemann@web.de>
2025-08-18 23:24:36 +0200

    feat: add WideCageLampBlock

---
+++ src/main/kotlin/grauly/dustydecor/block/WideCageLampBlock.kt
@@ -0,0 +1,21 @@
+package grauly.dustydecor.block
+
+import net.minecraft.block.BlockState
+import net.minecraft.state.property.Properties
+import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+
+class WideCageLampBlock(settings: Settings?) : FacingRotationLampBlock(settings) {
+    override fun getShape(state: BlockState): VoxelShape {
+        return (SHAPES[state.get(ROTATED)]!!)[state.get(Properties.FACING)]!!
+    }
+
+    companion object {
+        private val BASE_SHAPE: VoxelShape = VoxelShapes.cuboid(5.0/16, 4.0/16, 0.0, 11.0/16, 12.0/16, 5.0/16)
+        private val ROTATED_SHAPE: VoxelShape = VoxelShapes.cuboid(4.0/16, 5.0/16, 0.0, 12.0/16, 11.0/16, 5.0/16)
+        val SHAPES: Map<Boolean, Map<Direction, VoxelShape>> = (listOf(true, false).fold(mutableMapOf()) { acc, element ->
+            acc[element] = VoxelShapes.createFacingShapeMap(if (element) ROTATED_SHAPE else BASE_SHAPE); acc
+        })
+    }
+}
