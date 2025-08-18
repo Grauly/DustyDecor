8d184fde9c25c5202d6f54ac52a3d313a70ca5f1
Grauly <nils.dellemann@web.de>
2025-08-18 15:12:45 +0200

    feat: placement restrictions

--- src/main/kotlin/grauly/dustydecor/block/TallCageLampBlock.kt
+++ src/main/kotlin/grauly/dustydecor/block/TallCageLampBlock.kt
@@ -2,6 +2,7 @@ package grauly.dustydecor.block
 
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
 import net.minecraft.block.ShapeContext
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.state.StateManager
@@ -10,15 +11,47 @@ import net.minecraft.util.BlockMirror
 import net.minecraft.util.BlockRotation
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import net.minecraft.util.math.random.Random
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
+import net.minecraft.world.WorldView
+import net.minecraft.world.tick.ScheduledTickView
 
 class TallCageLampBlock(settings: Settings?) : LightingFixtureBlock(settings) {
     init {
         defaultState = defaultState
             .with(Properties.FACING, Direction.UP)
         //TODO: tinting
+    }
+
+    override fun getStateForNeighborUpdate(
+        state: BlockState,
+        world: WorldView,
+        tickView: ScheduledTickView,
+        pos: BlockPos,
+        direction: Direction,
+        neighborPos: BlockPos,
+        neighborState: BlockState,
+        random: Random
+    ): BlockState {
+        if (!canPlaceAt(state, world, pos)) return Blocks.AIR.defaultState
+        return super.getStateForNeighborUpdate(
+            state,
+            world,
+            tickView,
+            pos,
+            direction,
+            neighborPos,
+            neighborState,
+            random
+        )
+    }
+
+    override fun canPlaceAt(state: BlockState, world: WorldView, pos: BlockPos): Boolean {
+        val direction = state.get(Properties.FACING)
+        val checkState = world.getBlockState(pos.offset(direction.opposite))
+        return checkState.isSideSolidFullSquare(world, pos, direction)
     }
 
     override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
