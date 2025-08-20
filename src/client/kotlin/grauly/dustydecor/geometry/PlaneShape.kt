355914d3320771dbb110c508c1f4224aaeeddce9
Grauly <nils.dellemann@web.de>
2025-08-20 17:09:54 +0200

    fix: fix PlaneShape now working
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding
    I hate triangle winding

--- src/client/kotlin/grauly/dustydecor/geometry/PlaneShape.kt
+++ src/client/kotlin/grauly/dustydecor/geometry/PlaneShape.kt
@@ -9,15 +9,15 @@ object PlaneShape : ShapeDefinition {
     override fun getUvs(): List<Vec2f> = UVS
 
     private val POINTS: List<Vec3d> = listOf(
-        Vec3d(0.5, 0.0, -0.5),
-        Vec3d(0.5, 0.0, 0.5),
+        Vec3d(-0.5, 0.0, -0.5),
         Vec3d(-0.5, 0.0, 0.5),
-        Vec3d(-0.5, 0.0, -0.5)
+        Vec3d(0.5, 0.0, 0.5),
+        Vec3d(0.5, 0.0, -0.5),
     )
     private val UVS: List<Vec2f> = listOf(
-        Vec2f(0f, 0f),
-        Vec2f(1f, 0f),
         Vec2f(1f, 1f),
-        Vec2f(0f, 1f)
+        Vec2f(1f, 0f),
+        Vec2f(0f, 0f),
+        Vec2f(0f, 1f),
     )
 }
