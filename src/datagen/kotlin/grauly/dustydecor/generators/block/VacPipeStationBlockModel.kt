89ba03c0dacfd87067be09d21e776a1a41736eed
Grauly <nils.dellemann@web.de>
2025-09-08 18:01:38 +0200

    feat: updated vac pipe station model

--- src/datagen/kotlin/grauly/dustydecor/generators/block/VacPipeStationBlockModel.kt
+++ src/datagen/kotlin/grauly/dustydecor/generators/block/VacPipeStationBlockModel.kt
@@ -3,7 +3,6 @@ package grauly.dustydecor.generators.block
 import grauly.dustydecor.DustyDecorMod
 import grauly.dustydecor.ModBlocks
 import grauly.dustydecor.ModItems
-import grauly.dustydecor.block.VacPipeStationBlock
 import grauly.dustydecor.generators.BlockModelDatagen
 import net.minecraft.client.data.BlockStateModelGenerator
 import net.minecraft.client.data.MultipartBlockModelDefinitionCreator
@@ -16,28 +15,16 @@ object VacPipeStationBlockModel {
     fun get(blockStateModelGenerator: BlockStateModelGenerator) {
         val creator = MultipartBlockModelDefinitionCreator.create(ModBlocks.VAC_PIPE_STATION)
         listOf(Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST).forEach directionLoop@{ direction ->
-            val operator = BlockModelDatagen.NORTH_FACING_ROTATION_MAP[direction]
             creator.with(
                 MultipartModelConditionBuilder()
                     .put(Properties.FACING, direction),
                 MODEL
-                    .apply(operator)
+                    .apply(BlockModelDatagen.NORTH_FACING_ROTATION_MAP[direction])
             )
-            listOf(true, false).forEach sendingLoop@{ sending ->
-                creator.with(
-                    MultipartModelConditionBuilder()
-                        .put(Properties.FACING, direction)
-                        .put(VacPipeStationBlock.SENDING, sending),
-                    (if (sending) SEND_MODEL else RECEIVE_MODEL)
-                        .apply(operator)
-                )
-            }
         }
         blockStateModelGenerator.blockStateCollector.accept(creator)
         blockStateModelGenerator.registerItemModel(ModItems.VAC_PIPE_STATION, Identifier.of(DustyDecorMod.MODID, "block/vac_pipe_station"))
     }
 
     private val MODEL = BlockModelDatagen.singleVariant("block/vac_pipe_station/vac_pipe_station")
-    private val SEND_MODEL = BlockModelDatagen.singleVariant("block/vac_pipe_station/vac_pipe_station_sign_send")
-    private val RECEIVE_MODEL = BlockModelDatagen.singleVariant("block/vac_pipe_station/vac_pipe_station_sign_receive")
 }
