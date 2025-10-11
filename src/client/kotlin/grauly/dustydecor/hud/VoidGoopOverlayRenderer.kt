d26b16d202c37c8a97681f3b6129cdb7b11959f0
Grauly <nils.dellemann@web.de>
2025-10-11 22:22:55 +0200

    feat: proper void overlay

--- src/client/kotlin/grauly/dustydecor/hud/VoidGoopOverlayRenderer.kt
+++ src/client/kotlin/grauly/dustydecor/hud/VoidGoopOverlayRenderer.kt
@@ -7,75 +7,53 @@ import net.minecraft.client.gl.RenderPipelines
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.render.RenderTickCounter
 import net.minecraft.util.Identifier
-import org.joml.Vector2i
-import kotlin.math.sqrt
+import kotlin.math.*
 
 object VoidGoopOverlayRenderer {
     private val client = MinecraftClient.getInstance()
-    private val TEXTURE = Identifier.of(DustyDecorMod.MODID, "textures/misc/void_overlay/void_bubble.png")
-    private val FULL_TEXTURE = Identifier.of(DustyDecorMod.MODID, "textures/misc/void_overlay/void_bubble_full.png")
+    private val END_TEXTURE = Identifier.of(DustyDecorMod.MODID, "textures/misc/void_overlay/void_streak_end.png")
+    private val TEXTURE = Identifier.of(DustyDecorMod.MODID, "textures/misc/void_overlay/void_streak.png")
 
     fun render(context: DrawContext, tickCounter: RenderTickCounter) {
         val player = client.player ?: return
         val consumption = player.getAttachedOrElse(ModAttachmentTypes.VOID_CONSUMPTION, 0f)
         if (consumption <= 0.0) return
-        val gridSize = 30
-        val gridPointsX = ensureUneven((context.scaledWindowWidth / gridSize) + 1)
-        val gridPointsY = ensureUneven((context.scaledWindowHeight / gridSize) + 1)
-        val gridHalfX = (gridPointsX - 1) / 2
-        val gridHalfY = (gridPointsY - 1) / 2
-        val pointList = mutableListOf<Vector2i>()
-        for (x in (-gridHalfX)..gridHalfX) {
-            for (y in (-gridHalfY)..gridHalfY) {
-                val gridX = context.scaledWindowWidth / 2 + (x * gridSize)
-                val gridY = context.scaledWindowHeight / 2 + (y * gridSize)
-                pointList.add(Vector2i(gridX, gridY))
-                pointList.add(Vector2i(gridX + gridSize / 2, gridY + gridSize / 2))
+        val gridSize = 4
+        val gridPoints = context.scaledWindowWidth / gridSize
+        context.matrices.pushMatrix()
+        for (n in 0..1) {
+            if (n == 1) {
+                context.matrices.translate(
+                    context.scaledWindowWidth.toFloat(),
+                    context.scaledWindowHeight.toFloat()
+                )
+                context.matrices.rotate(PI.toFloat())
+            }
+            for (i in 1..gridPoints) {
+                val x = -2 + i * gridSize
+                val y = getOffset(context, consumption, x)
+                drawStreak(context, -y + context.scaledWindowHeight, x)
             }
         }
-        val center = Vector2i(context.scaledWindowWidth / 2, context.scaledWindowHeight / 2)
-        val fullDistance = center.distance(Vector2i(0, 0)) * 1.1
-        val fullAdjustedDistance = adjustedDistance(context, Vector2i(0, 0)) * 1.1
-        pointList.removeAll {
-            adjustedDistance(context, it) < fullAdjustedDistance * (1 - consumption)
-        }
-        //pointList.sortBy { it.distance(center) }
-        pointList.forEach {
-            val texture = if (
-                adjustedDistance(context, it) < fullAdjustedDistance * (1 - consumption + 0.05)
-                //it.distance(center) < fullDistance * (1 - consumption + 0.05)
-                ) {
-                FULL_TEXTURE
-            } else TEXTURE
-            drawCenteredBubble(context,texture, it.x, it.y)
+        context.matrices.popMatrix()
+    }
+
+    private fun drawStreak(context: DrawContext, targetY: Int, centerX: Int) {
+        val segmentCount: Int = floor(targetY / (8 * 2f)).toInt()
+        for (i in 0..segmentCount) {
+            context.drawTexture(
+                RenderPipelines.GUI_TEXTURED,
+                if (i == 0) END_TEXTURE else TEXTURE,
+                centerX - 2, targetY - (i + 1) * 8 * 2,
+                0f, 0f, 4, 16,
+                4, 16
+            )
         }
     }
 
-    private fun drawCenteredBubble(context: DrawContext, texture: Identifier, centerX: Int, centerY: Int) {
-        drawBubble(context, texture, centerX - 15, centerY - 15)
-    }
-
-    private fun drawBubble(context: DrawContext, texture: Identifier, x: Int, y: Int) {
-        //yes, 32x32 instead of 16x16, to better match the vanilla overlays
-        context.drawTexture(
-            RenderPipelines.GUI_TEXTURED,
-            texture,
-            x, y,
-            0f, 0f,
-            30, 30, 32, 32,
-            -1
-        )
-    }
-
-    private fun adjustedDistance(context: DrawContext, vector: Vector2i): Double {
-        val ratio = context.scaledWindowHeight / context.scaledWindowWidth.toDouble()
-        val a = vector.x - context.scaledWindowWidth / 2.0
-        val b = vector.y - context.scaledWindowHeight / 2.0
-        return sqrt(a * a * ratio + b * b)
-    }
-
-    private fun ensureUneven(number: Int): Int {
-        if (number % 2 == 1) return number
-        return number + 1
+    private fun getOffset(context: DrawContext, delta: Float, x: Int): Int {
+        val eyeprogress = max(0f, min(1f, (2 - delta * 2)))
+        val offset = max(0f, min(1f, 1 - delta * 2))
+        return ((sin(PI * x * (1f / context.scaledWindowWidth)).pow(2.0 + (1 - eyeprogress)) * .5 * eyeprogress * context.scaledWindowHeight + .5f * (1 + offset) * context.scaledWindowHeight).roundToInt())
     }
 }
