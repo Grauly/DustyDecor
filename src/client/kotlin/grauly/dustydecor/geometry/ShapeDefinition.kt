5fb8ae0b899650c1b9f1f32942852f2fb1ab4f67
Grauly <nils.dellemann@web.de>
2025-11-09 23:40:13 +0100

    feat: add direct rendering support for shapes

--- src/client/kotlin/grauly/dustydecor/geometry/ShapeDefinition.kt
+++ src/client/kotlin/grauly/dustydecor/geometry/ShapeDefinition.kt
@@ -1,5 +1,6 @@
 package grauly.dustydecor.geometry
 
+import grauly.dustydecor.particle.spark.QuadBasedParticleSubmittable
 import net.minecraft.client.render.VertexConsumer
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.math.Vec2f
@@ -36,5 +37,21 @@ interface ShapeDefinition {
     }
     fun transformPoint(point: Vec3d, posOffset: Vec3d, scale: Vec3d, rotation: Quaternionf): Vec3d {
         return Vec3d(point.toVector3f().mul(scale.x.toFloat(), scale.y.toFloat(), scale.z.toFloat()).rotate(rotation).add(posOffset.toVector3f()))
+    }
+    fun submitUniformly(submittable: QuadBasedParticleSubmittable, packedLight: Int, color: Int) {
+        if (getPoints().size % 4 != 0) throw IllegalStateException("Non Quad Shape cannot be submitted to QuadBasedParticleSubmittable")
+        var counter = 0
+        for (i in 0..<getPoints().size) {
+            if (counter == 0) {
+                submittable.beginQuad()
+            }
+            val uv = getUvs()[i]
+            submittable.addVertex(getPoints()[i].toVector3f(), uv.x, uv.y, packedLight, color)
+            counter++
+            if (counter == 4) {
+                submittable.endQuad()
+                counter = 0
+            }
+        }
     }
 }
