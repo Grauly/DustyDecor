c682675e39469bc0f2b89c706dd62d8ce49e27e3
Grauly <nils.dellemann@web.de>
2025-11-12 02:55:36 +0100

    fix: finally fix the Spark Particle
    Holy shit this was annoying

--- src/client/kotlin/grauly/dustydecor/particle/spark/QuadBasedParticleSubmittable.kt
+++ src/client/kotlin/grauly/dustydecor/particle/spark/QuadBasedParticleSubmittable.kt
@@ -1,103 +1,34 @@
 package grauly.dustydecor.particle.spark
 
-import com.mojang.blaze3d.systems.RenderPass
-import com.mojang.blaze3d.systems.RenderSystem
-import com.mojang.blaze3d.vertex.VertexFormat.DrawMode
 import grauly.dustydecor.particle.QuadDataCache
 import net.minecraft.client.particle.BillboardParticle
-import net.minecraft.client.particle.BillboardParticleSubmittable
-import net.minecraft.client.render.BufferBuilder
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.RenderPhase
 import net.minecraft.client.render.Submittable
 import net.minecraft.client.render.VertexFormats
-import net.minecraft.client.render.command.LayeredCustomCommandRenderer
 import net.minecraft.client.render.command.OrderedRenderCommandQueue
 import net.minecraft.client.render.state.CameraRenderState
-import net.minecraft.client.texture.TextureManager
-import net.minecraft.client.util.BufferAllocator
+import net.minecraft.client.util.math.MatrixStack
 import org.joml.Vector3f
-import org.joml.Vector4f
 
 class QuadBasedParticleSubmittable(
     initialBufferSize: Int = 128
-) : Submittable, OrderedRenderCommandQueue.LayeredCustom {
+) : Submittable {
 
     private var dataCache = QuadDataCache(initialBufferSize)
 
-    //OrderedRenderCommandQueue
-    override fun submit(cache: LayeredCustomCommandRenderer.VerticesCache): BillboardParticleSubmittable.Buffers? {
-        BufferAllocator.fixedSized(dataCache.getWrittenQuads() * 4 * VertexFormats.POSITION_COLOR_TEXTURE_LIGHT.vertexSize)
-            .use { bufferAllocator ->
-                val bufferBuilder =
-                    BufferBuilder(bufferAllocator, DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT)
-                dataCache.forEachVertex { pos, u, v, light, color ->
-                    bufferBuilder
-                        .vertex(pos)
-                        .texture(u, v)
-                        .color(color)
-                        .light(light)
-                }
-                val returnMap: Map<BillboardParticle.RenderType, BillboardParticleSubmittable.Layer> = mapOf(
-                    (BillboardParticle.RenderType.PARTICLE_ATLAS_OPAQUE to BillboardParticleSubmittable.Layer(
-                        0,
-                        dataCache.getWrittenQuads() * 6
-                    ))
-                )
-                val builtBuffer = bufferBuilder.endNullable() ?: return null
-                cache.write(builtBuffer.buffer)
-                RenderSystem.getSequentialBuffer(DrawMode.QUADS).getIndexBuffer(builtBuffer.drawParameters.indexCount())
-                val gpuBufferSlice = RenderSystem.getDynamicUniforms()
-                    .write(
-                        RenderSystem.getModelViewMatrix(),
-                        Vector4f(1f, 1f, 1f, 1f),
-                        Vector3f(),
-                        RenderSystem.getTextureMatrix(),
-                        RenderSystem.getShaderLineWidth()
-                    )
-                return BillboardParticleSubmittable.Buffers(builtBuffer.drawParameters.indexCount(), gpuBufferSlice, returnMap)
-            }
-    }
-
-    //Submittable
     override fun submit(
         queue: OrderedRenderCommandQueue,
         cameraRenderState: CameraRenderState
     ) {
-        if (dataCache.getWrittenQuads() > 0) {
-            queue.submitCustom(this)
-        }
-    }
-
-    //OrderedRenderCommandQueue
-    override fun render(
-        buffers: BillboardParticleSubmittable.Buffers,
-        cache: LayeredCustomCommandRenderer.VerticesCache,
-        renderPass: RenderPass,
-        manager: TextureManager,
-        translucent: Boolean
-    ) {
-        val shapeIndexBuffer = RenderSystem.getSequentialBuffer(DrawMode.QUADS)
-        renderPass.setVertexBuffer(0, cache.get())
-        renderPass.setIndexBuffer(
-            shapeIndexBuffer.getIndexBuffer(buffers.indexCount),
-            shapeIndexBuffer.indexType
-        )
-        renderPass.setUniform("DynamicTransforms", buffers.dynamicTransforms)
-
-        for (entry in buffers.layers.entries) {
-            if (translucent == (entry.key as BillboardParticle.RenderType).translucent()) {
-                renderPass.setPipeline((entry.key as BillboardParticle.RenderType).pipeline())
-                renderPass.bindSampler(
-                    "Sampler0",
-                    manager
-                        .getTexture((entry.key as BillboardParticle.RenderType).textureAtlasLocation())
-                        .getGlTextureView()
-                )
-                renderPass.drawIndexed(
-                    (entry.value as BillboardParticleSubmittable.Layer).vertexOffset,
-                    0,
-                    (entry.value as BillboardParticleSubmittable.Layer).indexCount,
-                    1
-                )
+        if (dataCache.getWrittenQuads() <= 0) return
+        val matrices = MatrixStack()
+        queue.submitCustom(
+            matrices,
+            RENDER_LAYER
+        ) { matrixEnty, vertexConsumer ->
+            dataCache.forEachVertex { pos, u, v, light, color ->
+                vertexConsumer.vertex(pos).texture(u, v).light(light).color(color)
             }
         }
     }
@@ -123,8 +54,27 @@ class QuadBasedParticleSubmittable(
     ) {
         dataCache.insertVertex(
             pos.x, pos.y, pos.z,
-            u,v,
+            u, v,
             light, color
+        )
+    }
+
+    companion object {
+        private val RENDER_LAYER = RenderLayer.of(
+            "dustydecor_spark_particle",
+            VertexFormats.POSITION_COLOR_TEXTURE_LIGHT.vertexSize,
+            false,
+            false,
+            BillboardParticle.RenderType.PARTICLE_ATLAS_OPAQUE.pipeline,
+            RenderLayer.MultiPhaseParameters.builder()
+                .texture(
+                    RenderPhase.Texture(
+                        BillboardParticle.RenderType.PARTICLE_ATLAS_OPAQUE.textureAtlasLocation,
+                        false
+                    )
+                )
+                .lightmap(RenderPhase.ENABLE_LIGHTMAP)
+                .build(false)
         )
     }
 }
