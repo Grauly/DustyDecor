ca470ac540dad7a4d09c5af1941ae0e6e8232081
Grauly <nils.dellemann@web.de>
2026-02-01 23:09:53 +0100

    refactor: update CreateiveModeTabs to mojmap

---
+++ src/main/kotlin/grauly/dustydecor/ModCreativeModeTabs.kt
@@ -0,0 +1,42 @@
+package grauly.dustydecor
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents
+import net.minecraft.world.item.CreativeModeTabs
+import net.minecraft.world.item.Items
+
+object ModCreativeModeTabs {
+    fun init() {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.BUILDING_BLOCKS)
+            .register {
+                it.insertAfter(Items.IRON_CHAIN, ModItems.VENT, ModItems.VENT_COVER)
+            }
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
+            .register {
+                it.insertAfter(Items.BRUSH, ModItems.SCREWDRIVER, ModItems.WRENCH)
+                it.insertAfter(Items.PINK_BUNDLE, ModItems.VAC_CAPSULE)
+            }
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+            .register {
+                it.insertAfter(Items.WAXED_OXIDIZED_COPPER_CHEST, ModItems.VAC_PIPE, ModItems.VAC_PIPE_STATION)
+            }
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.COLORED_BLOCKS)
+            .register {
+                it.insertAfter(
+                    Items.PINK_CANDLE,
+                    *ModItems.TALL_CAGE_LAMPS.toTypedArray(),
+                    *ModItems.WIDE_CAGE_LAMPS.toTypedArray(),
+                    *ModItems.ALARM_CAGE_LAMPS.toTypedArray(),
+                    *ModItems.TUBE_LAMPS.toTypedArray(),
+                    *ModItems.STOOLS.toTypedArray()
+                )
+            }
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS)
+            .register {
+                it.insertAfter(Items.POPPED_CHORUS_FRUIT, ModItems.OUTSIDE_CRYSTAL_SHARD)
+            }
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.NATURAL_BLOCKS)
+            .register {
+                it.insertAfter(Items.SCULK_SENSOR, ModItems.VOID_GOOP, ModItems.BULK_VOID_GOOP)
+            }
+    }
+}
