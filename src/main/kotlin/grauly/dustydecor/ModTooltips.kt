512e701dea847af0abbdaba448f0ae4d8d8eca13
Grauly <nils.dellemann@web.de>
2026-02-01 23:20:45 +0100

    fix: more FAPI renaming

--- src/main/kotlin/grauly/dustydecor/ModTooltips.kt
+++ src/main/kotlin/grauly/dustydecor/ModTooltips.kt
@@ -1,12 +1,13 @@
 package grauly.dustydecor
 
-import net.fabricmc.fabric.api.item.v1.ComponentTooltipAppenderRegistry
+import net.fabricmc.fabric.api.item.v1.ItemComponentTooltipProviderRegistry
+
 
 object ModTooltips {
 
     fun init() {
-        ComponentTooltipAppenderRegistry.addFirst(ModDataComponentTypes.WRENCH)
-        ComponentTooltipAppenderRegistry.addFirst(ModDataComponentTypes.SCREWDRIVER)
-        ComponentTooltipAppenderRegistry.addFirst(ModDataComponentTypes.VOID_GOOP_SIZE)
+        ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.WRENCH)
+        ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.SCREWDRIVER)
+        ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.VOID_GOOP_SIZE)
     }
 }
