7bfe812bfc49bda954fef28747971b3f4bf17c07
Grauly <nils.dellemann@web.de>
2026-02-05 23:31:38 +0100

    feat: add proper stool rotations
    Yes, its suboptimal, but oh well

--- src/datagen/kotlin/grauly/dustydecor/generators/block/SingleFurnitureBlockModel.kt
+++ src/datagen/kotlin/grauly/dustydecor/generators/block/SingleFurnitureBlockModel.kt
@@ -1,5 +1,6 @@
 package grauly.dustydecor.generators.block
 
+import com.mojang.math.Quadrant
 import grauly.dustydecor.DustyDecorMod
 import grauly.dustydecor.block.furniture.SingleFurnitureBlock
 import grauly.dustydecor.generators.BlockModelDatagen
@@ -7,7 +8,11 @@ import grauly.dustydecor.util.DyeUtils
 import net.minecraft.client.data.models.BlockModelGenerators
 import net.minecraft.client.data.models.model.ItemModelUtils
 import net.minecraft.client.data.models.blockstates.MultiVariantGenerator
+import net.minecraft.client.data.models.blockstates.PropertyDispatch
+import net.minecraft.client.renderer.block.model.VariantMutator
 import net.minecraft.resources.Identifier
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import kotlin.math.floor
 
 open class SingleFurnitureBlockModel(private val blocks: List<SingleFurnitureBlock>, private val modelPrefix: String) {
     fun get(blockStateModelGenerator: BlockModelGenerators) {
@@ -23,18 +28,22 @@ open class SingleFurnitureBlockModel(private val blocks: List<SingleFurnitureBlo
 
     protected fun createBlock(block: SingleFurnitureBlock, blockStateModelGenerator: BlockModelGenerators) {
         blockStateModelGenerator.blockStateOutput.accept(
-            MultiVariantGenerator.dispatch(block, MODEL)
-            //TODO: actual rotations
+            MultiVariantGenerator.dispatch(block).with(PropertyDispatch.initial(BlockStateProperties.ROTATION_16).generate {
+                val rotationIndex = it % 4
+                val baseRotation = floor(it / 4.0).toInt()
+                val rotationQuadrant = Quadrant.entries[baseRotation]
+                getModel(rotationIndex).with(VariantMutator.Y_ROT.withValue(rotationQuadrant))
+            })
         )
     }
 
     protected fun createItem(block: SingleFurnitureBlock, blockStateModelGenerator: BlockModelGenerators) {
         val color = DyeUtils.COLOR_ORDER[blocks.indexOf(block)].textColor
         val tint = ItemModelUtils.constantTint(color)
-        val model = ItemModelUtils.tintedModel(Identifier.fromNamespaceAndPath(DustyDecorMod.MODID, MODEL_PATH), tint)
+        val model = ItemModelUtils.tintedModel(Identifier.fromNamespaceAndPath(DustyDecorMod.MODID, "${MODEL_PATH}_0"), tint)
         blockStateModelGenerator.itemModelOutput.accept(block.asItem(), model)
     }
 
     protected val MODEL_PATH = "block/$modelPrefix"
-    protected val MODEL = BlockModelDatagen.singleVariant(MODEL_PATH)
+    private fun getModel(rotationOffset: Int) = BlockModelDatagen.singleVariant("${MODEL_PATH}_$rotationOffset")
 }
