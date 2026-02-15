dd4f4c15036e4c0ff9b7a185b33009c33c0fcfbd
Grauly <nils.dellemann@web.de>
2026-02-15 22:21:12 +0100

    feat: tooltip insertions for tool components

--- src/main/kotlin/grauly/dustydecor/ModTooltips.kt
+++ src/main/kotlin/grauly/dustydecor/ModTooltips.kt
@@ -6,9 +6,9 @@ object ModTooltips {
 
     fun init() {
         ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.WRENCH)
-        ItemComponentTooltipProviderRegistry.addLast(ModDataComponentTypes.VENT_COVER_LOCK)
-        ItemComponentTooltipProviderRegistry.addLast(ModDataComponentTypes.VAC_TUBE_WINDOW_TOGGLE)
-        ItemComponentTooltipProviderRegistry.addLast(ModDataComponentTypes.LAMP_INVERSION)
+        ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.VENT_COVER_LOCK)
+        ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.VAC_TUBE_WINDOW_TOGGLE)
+        ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.LAMP_INVERSION)
         ItemComponentTooltipProviderRegistry.addFirst(ModDataComponentTypes.VOID_GOOP_SIZE)
     }
 }
--- src/main/kotlin/grauly/dustydecor/component/ToolComponents.kt
+++ src/main/kotlin/grauly/dustydecor/component/ToolComponents.kt
@@ -3,7 +3,9 @@ package grauly.dustydecor.component
 import com.mojang.serialization.Codec
 import com.mojang.serialization.MapCodec
 import grauly.dustydecor.DustyDecorMod
+import grauly.dustydecor.ModBlocks
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.network.chat.Style
 import net.minecraft.network.chat.TextColor
 import net.minecraft.util.CommonColors
@@ -11,12 +13,12 @@ import net.minecraft.world.item.component.TooltipProvider
 
 object ToolComponents {
 
-    val VENT_LOCK_TOGGLE = makeToolComponent("component.screwdriver.${DustyDecorMod.MODID}.tooltip.vent_cover")
-    val VAC_TUBE_WINDOW_TOGGLE = makeToolComponent("component.screwdriver.${DustyDecorMod.MODID}.tooltip.vac_tube")
+    val VENT_LOCK_TOGGLE = makeToolComponent("component.screwdriver.${DustyDecorMod.MODID}.tooltip.vent_cover", ModBlocks.VENT_COVER.name)
+    val VAC_TUBE_WINDOW_TOGGLE = makeToolComponent("component.screwdriver.${DustyDecorMod.MODID}.tooltip.vac_tube", ModBlocks.VAC_PIPE.name)
     val LAMPS_INVERT = makeToolComponent("component.screwdriver.${DustyDecorMod.MODID}.tooltip.")
 
-    fun makeToolComponent(translationKey: String): ToolComponent {
-        val component = Component.translatable(translationKey).withStyle(Style.EMPTY.withColor(TextColor.fromRgb(CommonColors.LIGHT_GRAY)))
+    fun makeToolComponent(translationKey: String, vararg insertions: Component): ToolComponent {
+        val component = Component.translatable(translationKey, *insertions).withStyle(Style.EMPTY.withColor(TextColor.fromRgb(CommonColors.LIGHT_GRAY)))
         val provider = TooltipProvider { context, consumer, flag, components -> consumer.accept(component)}
         val codec = MapCodec.unitCodec(provider)
         return ToolComponent(translationKey, provider, codec)
