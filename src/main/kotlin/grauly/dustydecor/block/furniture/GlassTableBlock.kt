a1298e86b4a768a2a0640dc21a2fabada128bea6
Grauly <nils.dellemann@web.de>
2026-03-01 13:35:46 +0100

    feat: implement ImpactBreakable in GlassTableBlock

--- src/main/kotlin/grauly/dustydecor/block/furniture/GlassTableBlock.kt
+++ src/main/kotlin/grauly/dustydecor/block/furniture/GlassTableBlock.kt
@@ -3,10 +3,12 @@ package grauly.dustydecor.block.furniture
 import grauly.dustydecor.ModBlocks
 import grauly.dustydecor.ModDataComponentTypes
 import grauly.dustydecor.ModSoundEvents
+import grauly.dustydecor.block.ImpactBreakable
 import grauly.dustydecor.util.DyeUtils
 import grauly.dustydecor.util.GlassUtils
 import grauly.dustydecor.util.ToolUtils
 import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.sounds.SoundSource
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
@@ -19,12 +21,13 @@ import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.gameevent.GameEvent
+import net.minecraft.world.level.storage.loot.LootParams
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.Shapes
 import net.minecraft.world.phys.shapes.VoxelShape
 
-class GlassTableBlock(settings: Properties) : RestrictedRotationFurnitureBlock(settings) {
+class GlassTableBlock(settings: Properties) : RestrictedRotationFurnitureBlock(settings), ImpactBreakable {
 
     override fun useItemOn(
         itemStack: ItemStack,
@@ -36,18 +39,28 @@ class GlassTableBlock(settings: Properties) : RestrictedRotationFurnitureBlock(s
         hitResult: BlockHitResult
     ): InteractionResult {
         if (itemStack.has(ModDataComponentTypes.SMALL_GLASS_TABLE_STRIP_PANE)) {
+            if (level !is ServerLevel) return super.useItemOn(itemStack, state, level, pos, player, hand, hitResult)
             ToolUtils.playToolSound(itemStack, pos, level, player)
-            replaceWithFrame(state, level, pos)
-            level.playSound(
-                player,
-                pos,
-                ModSoundEvents.BLOCK_VAP_PIPE_REMOVE_WINDOW,
-                SoundSource.BLOCKS
-            )
+            repair(level, state, pos)
             player.inventory.placeItemBackInInventory(getGlassState(state.block).block.asItem().defaultInstance.copyWithCount(1))
             return InteractionResult.SUCCESS
         }
         return super.useItemOn(itemStack, state, level, pos, player, hand, hitResult)
+    }
+
+    override fun onRepair(level: ServerLevel, state: BlockState, pos: BlockPos) {
+        replaceWithFrame(state, level, pos)
+        level.playSound(
+            null,
+            pos,
+            ModSoundEvents.BLOCK_VAP_PIPE_REMOVE_WINDOW,
+            SoundSource.BLOCKS
+        )
+    }
+
+    override fun attack(state: BlockState, level: Level, pos: BlockPos, player: Player) {
+        super.attack(state, level, pos, player)
+        onAttacked(state, level, pos, player)
     }
 
     override fun onProjectileHit(
@@ -56,16 +69,22 @@ class GlassTableBlock(settings: Properties) : RestrictedRotationFurnitureBlock(s
         blockHit: BlockHitResult,
         projectile: Projectile
     ) {
-        replaceWithFrame(state, level, blockHit.blockPos)
+        onProjectileImpact(level, state, blockHit, projectile)
+    }
+
+    override fun getParticleEffectMultiplier(): Int = 5
+
+    override fun onBroken(level: ServerLevel, state: BlockState, pos: BlockPos) {
         level.playSound(
             null,
-            blockHit.blockPos,
-            getSoundType(state).breakSound,
+            pos,
+            state.soundType.breakSound,
             SoundSource.BLOCKS
         )
-        for (i in 0..5) {
-            level.addDestroyBlockEffect(blockHit.blockPos, getGlassState(state.block))
+        for (i in 0..getParticleEffectMultiplier()) {
+            playBreakParticleEffect(level, getGlassState(this), pos)
         }
+        replaceWithFrame(state, level, pos)
     }
 
     private fun replaceWithFrame(
