f3221d8ec9169b7a2bd16ed12f1d88fc3d777e12
Grauly <nils.dellemann@web.de>
2026-03-29 03:03:04 +0200

    fix: prevent world load ringing sync

--- src/main/kotlin/grauly/dustydecor/blockentity/PhoneBlockEntity.kt
+++ src/main/kotlin/grauly/dustydecor/blockentity/PhoneBlockEntity.kt
@@ -1,6 +1,5 @@
 package grauly.dustydecor.blockentity
 
-import grauly.dustydecor.DustyDecorMod
 import grauly.dustydecor.ModBlockEntityTypes
 import grauly.dustydecor.ModSoundEvents
 import grauly.dustydecor.block.furniture.PhoneBlock
@@ -13,15 +12,16 @@ import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.Vec3
+import java.util.*
 import kotlin.math.max
 
 class PhoneBlockEntity(
     worldPosition: BlockPos,
     blockState: BlockState
 ) : BlockEntity(ModBlockEntityTypes.PHONE_ENTITY, worldPosition, blockState), ItemOwner {
-    var pauseTicks = 0;
-    var ringTicks = 0;
-    val ringCycle = 4;
+    var pauseTicks = INIT_RANDOM.nextInt(RING_PAUSE_TICKS)
+    var ringTicks = -1
+    val ringCycle = 4
 
     override fun level(): Level = level!!
 
@@ -31,6 +31,11 @@ class PhoneBlockEntity(
         -blockState.getValue(SingleFurnitureBlock.ROTATION) * (360f / 16f)
 
     companion object {
+        const val RING_TICKS = 40
+        const val RING_PAUSE_TICKS = 80
+
+        val INIT_RANDOM = Random(System.currentTimeMillis())
+
         fun tick(level: Level, pos: BlockPos, state: BlockState, entity: PhoneBlockEntity) {
             if (state.getValue(PhoneBlock.RINGING)) {
                 tickRing(level, pos, state, entity)
@@ -52,11 +57,11 @@ class PhoneBlockEntity(
             }
             if (entity.ringTicks > 0) entity.ringTicks = max(entity.ringTicks - 1, 0)
             if (entity.ringTicks == 0) {
-                entity.pauseTicks = 4*20
+                entity.pauseTicks = RING_PAUSE_TICKS
                 entity.ringTicks = -1
             }
             if (entity.pauseTicks == 0) {
-                entity.ringTicks = 40
+                entity.ringTicks = RING_TICKS
                 entity.pauseTicks = -1
                 level.playLocalSound(
                     pos,
